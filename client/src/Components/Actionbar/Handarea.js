bbd81cd5bfac496ddcbe8814c4a61b1583022310
Marcel Wack <wac.marcel@gmail.com>
2021-12-22 19:16:03 +0100

    working on card handling

--- client/src/Components/Actionbar/Handarea.js
+++ client/src/Components/Actionbar/Handarea.js
@@ -1,42 +1,89 @@
-import React, { useRef } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import styled from "styled-components";
 import Card from "../utils/CardComponent";
+import { Draggable, selector } from "gsap/all";
+import gsap from "gsap";
+gsap.registerPlugin(Draggable);
 
-const CardWrapper = (props) => {
+const CardSlot = React.forwardRef((props, ref) => {
+  //get suit and value of the card from props.hand
+  var { suit, value } = props.hand[props.cardnumber];
   return (
-    <div className={props.className}>
-      <Card suit={props.suit} value={props.value} width={props.width} />
+    <div className={props.className + ` draggable`} ref={ref}>
+      <Card suit={suit} value={value} width={props.width} className="card" />
     </div>
   );
-};
+});
 
-const StyledCardWrapper = styled(CardWrapper)`
-    width: minmax(${(props) => props.width / 2}em, ${(props) => props.width}em);
-    position: absolute;
-    top: -5em;
-    left: ${(props) => {
-      return `calc( ${props.offset} * ${props.cardnumber} + ${props.padding.left_right}vw)`;
-    }};
-  }
+const StyledCardSlot = styled(CardSlot)`
+  width: minmax(${(props) => props.width / 2}em, ${(props) => props.width}em);
+  position: absolute;
+  top: -5em;
+  left: ${(props) => {
+    return `calc( ${props.offset} * ${props.cardnumber} + ${props.padding.left_right}vw)`;
+  }};
 `;
 
 const HandArea = (props) => {
   const handRef = useRef(null);
+  const cardRef = useRef([]);
+  const curRef = useRef(null);
+
+  const [hand, setHand] = useState(props.hand);
+
+  var dragElements = cardRef.current;
+
+  function onDrop(dragged, dropped) {
+    console.log(dragged, dropped);
+  }
+
+  Draggable.create(dragElements, {
+    bounds: handRef.current,
+    onDrag: (e) => {
+      var i = dragElements.length;
+      while (--i > -1) {
+        if (curRef.current.hitTest(dragElements[i], "10%")) {
+          console.log("hit", i);
+        }
+      }
+    },
+    onDragEnd: (e) => {
+      var i = dragElements.length;
+      while (--i > -1) {
+        if (curRef.current.hitTest(dragElements[i], "10%")) {
+          onDrop(curRef, dragElements[i]);
+        }
+      }
+    },
+    onDragStart(e) {
+      console.log("drag start");
+      curRef.current = this;
+    },
+  });
+
+  useEffect(() => {
+    console.log(cardRef.current);
+  }, []);
+
   return (
     <div className={props.className} ref={handRef}>
-      {props.hand.map((card, i) => {
+      {hand.map((card, i) => {
         return (
           // <div className="card_container" key={i}>
           //   <Card suit={card.suit} value={card.value} width={cardwith} />
           // </div>
-          <StyledCardWrapper
+          <StyledCardSlot
             key={i}
             cardnumber={i}
-            suit={card.suit}
-            value={card.value}
+            ref={(ele) => {
+              cardRef.current[i] = ele;
+            }}
+            hand={hand}
+            setHand={setHand}
             offset={props.offset}
             width={props.cardwidth}
             padding={props.padding}
+            handRef={handRef}
           />
         );
       })}
